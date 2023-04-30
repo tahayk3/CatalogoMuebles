666776b6cd2f37db0e64089e9c172779ea67ee39
tahayk3 <tahayk3@gmail.com>
2023-04-29 20:07:06 -0600

    añadiendo mueble

--- src/dataText.js
+++ src/dataText.js
@@ -4,4 +4,5 @@ export default[
     {id: 'mueble 3', precio: '1375', medidas: '180cm x 40cm x 80cm', colores: '⬜️blanco, 🟫cafe, 🟨nogal cenizo'},
     {id: 'mueble 4', precio: '600', medidas: '120cm x 40cm x 40cm',  colores: '⬜️blanco, ⬛negro'},
     {id: 'mueble 5', precio: '625, con patitas +Q35, con rodos +Q50', medidas: '156cm x 30cm x 80cm',  colores:' ⬜️blanco, 🟫cafe, 🟨nogal cenizo, 🔳combinado'},
+    {id: 'mueble 6', precio: '650', medidas: '93cm x 130cm x 40cm',  colores:' ⬜️blanco'},
 ];
