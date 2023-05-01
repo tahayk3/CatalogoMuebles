9745ed921c1c767c48296cdfebdf2702fcaff841
tahayk3 <tahayk3@gmail.com>
2023-05-01 16:11:13 -0600

    integrando emailjs

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,30 @@
-import React from "react";
 import './contact.css'
+
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+
 import {MdAttachEmail} from 'react-icons/md'
 import {SiMessenger} from 'react-icons/si'
 import {BsWhatsapp} from 'react-icons/bs'
 
 
-
 function Contact(){
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8vesmij', 'template_cs3xehi', form.current, 'qQesFgTGowxsLTGYI')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  };
+
     return (
         <div className='contact'>
             <h1>Comunícate con nosotros</h1>
@@ -36,10 +54,10 @@ function Contact(){
                     
                 </div>
 
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name="name" placeholder="Nombre y apellido" required/>
-                    <input type="email" name="correo" placeholder="Correo electronico" required/>
-                    <textarea name="message" id="" cols="30" rows="7" placeholder="Consulta" required></textarea>
+                    <input type="email" name="email" placeholder="Correo electronico" required/>
+                    <textarea name="message" cols="30" rows="7" placeholder="Consulta" required></textarea>
                     <button type="submit" className="btn-send">Enviar</button>
                 </form>
                 
